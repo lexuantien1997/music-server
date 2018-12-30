627a66a0d8468acf9d51a9aca54ed0d8989e09d6
Lê Xuân Tiến <tienlx97@gmail.com>
2018-12-30 16:56:14 +0700

    search song

--- configs/mongoose.config.js
+++ configs/mongoose.config.js
@@ -6,12 +6,12 @@ dotenv.config();
 module.exports = () => {
   // Database config
     mongoose
-        .connect(process.env.MONGO_ONLINE, { useNewUrlParser: true }, function(err){
+        .connect(process.env.MONGO_OFFLINE||process.env.MONGO_ONLINE, { useNewUrlParser: true }, function(err){
           if(err){
             console.log(err)
           }
           else{
-            console.log("Successfully connected to mlab")
+            console.log("Successfully connected to mlab || OFFLINE)
           }
         }) //process.env.MONGO_OFFLINE ||
   }
