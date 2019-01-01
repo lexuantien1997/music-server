aa81d70c5ed19a5c0984738f950f908e980fef33
DucThong <thongnguyen230497@gmail.com>
2019-01-01 09:37:39 +0700

    change search user

--- search_modules/search.user.js
+++ search_modules/search.user.js
@@ -8,25 +8,49 @@ module.exports = (req,res) => {
     let listUser = []
     console.log("user search string:", searchStr);
     
-    Database.User.find({}, function(err, users){
-        if(err){
+    // Database.User.find({}, function(err, users){
+    //     if(err){
 
+    //     }
+    //     else{
+    //         console.log(users)
+    //         for(let i = 0; i < users.length; i++){
+    //             if(users[i].name.toLowerCase().includes(searchStr.toLowerCase()) || users[i].userName.toLowerCase().includes(searchStr.toLowerCase())){
+    //                 let userInfo ={
+    //                     name: users[i].name,
+    //                     DoB: users[i].DoB,
+    //                     address: users[i].address,
+    //                     numberOfFollower: users[i].follower.length,
+    //                     followed: false, 
+    //                 }
+    //                 listUser.push(userInfo)
+    //             }
+    //         }
+    //         res.status(200).json({ err: 0, msg: "Success", users: listUser})
+    //     }
+    // })
+
+
+    let regexSearch = {
+        userName: {
+          $regex: `(?i)\w*${searchStr}`, // regex uppercase and lowercase maybe
+          $options: 'g'
+        },
+      }
+    
+      let projection = {
+        follower: { // just get 4 user follow
+          $slice: 10
+        },
+        following:{
+            $slice: 10
         }
-        else{
-            console.log(users)
-            for(let i = 0; i < users.length; i++){
-                if(users[i].name.toLowerCase().includes(searchStr.toLowerCase()) || users[i].userName.toLowerCase().includes(searchStr.toLowerCase())){
-                    let userInfo ={
-                        name: users[i].name,
-                        DoB: users[i].DoB,
-                        address: users[i].address,
-                        numberOfFollower: users[i].follower.length,
-                        followed: false, 
-                    }
-                    listUser.push(userInfo)
-                }
-            }
-            res.status(200).json({ err: 0, msg: "Success", users: listUser})
-        }
-    })
+      }
+    
+      Database.User
+      .find(regexSearch, projection)
+      .select("-password -userName -_id -downloadedMusic -likedMusics -listenHistory") // exclude password, _id, follower => get all field
+      .limit(10) // only get 7 data
+      .lean()
+      .then(data => res.status(200).json({ err: null,msg: "Success", count: data.length,data})); // response data
   }
